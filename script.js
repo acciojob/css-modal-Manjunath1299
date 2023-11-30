346cabc5e3a301e6f69e29c30fe617c191ab606b
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-11-30 21:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+// Get the modal element
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('openModal');
+
+// Get the close button
+var span = document.getElementsByClassName('close-modal')[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+// When the user clicks on the close button or outside the modal, close it
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
